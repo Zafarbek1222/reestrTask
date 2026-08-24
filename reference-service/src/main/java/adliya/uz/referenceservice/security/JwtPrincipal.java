f838f47cb678bc30937bc74abf53a0d663c97931
Patrick <doniyorkhudayarov@gmail.com>
2026-08-24 11:34:49 +0500

    remove additional unused declarations

--- reference-service/src/main/java/adliya/uz/referenceservice/security/JwtPrincipal.java
+++ reference-service/src/main/java/adliya/uz/referenceservice/security/JwtPrincipal.java
@@ -1,7 +1,4 @@
 package adliya.uz.referenceservice.security;
 
 public record JwtPrincipal(String email, String role) {
-    public boolean isSuperAdmin() {
-        return "ROLE_SUPER_ADMIN".equals(role);
-    }
 }
